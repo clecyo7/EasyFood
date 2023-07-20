c9fd7836327c86bb9257290f9e37176ab0dbaa23
clecyo7 <clecyocrf@gmail.com>
2023-07-20 14:30:04 -0300

    fix: Resources Api

--- app/Http/Resources/EvaluationResource.php
+++ app/Http/Resources/EvaluationResource.php
@@ -18,8 +18,8 @@ class EvaluationResource extends JsonResource
             'stars' => $this->stars,
             'comment' => $this->comment,
             'client' => new ClientResource($this->client),
-             'order' => new OrderResource($this->order),
+          //   'order' => new OrderResource($this->order),
         ];
-        
+
     }
 }
--- app/Http/Resources/OrderResource.php
+++ app/Http/Resources/OrderResource.php
@@ -27,7 +27,7 @@ class OrderResource extends JsonResource
              'client' => $this->client_id ? new ClientResource($this->client) : '',
              'table' => $this->table_id ? new TableResource($this->table) : '',
              'products' => ProductResource::collection($this->products),
-         //   'evaluations' => EvaluationResource::collection($this->evaluations),
+            'evaluations' => EvaluationResource::collection($this->evaluations),
         ];
     }
 }
